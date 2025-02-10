22eb5659a6c4c096a09464eafdb9ccb90016f25e
Dmytro Trofimov <dima6836753@gmail.com>
2025-02-10 10:13:14 +0100

    remove tests

--- src/test/java/com/certTrack/NotificationService/Controllers/NotificationControllerTest.java
+++ src/test/java/com/certTrack/NotificationService/Controllers/NotificationControllerTest.java
@@ -1,19 +1,11 @@
 package com.certTrack.NotificationService.Controllers;
 
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.certTrack.NotificationService.DTO.ResponseMessage;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -21,16 +13,8 @@ class NotificationControllerTest {
 
 	@Autowired
 	MockMvc api;
-	
-	@Autowired
-	ObjectMapper objectMapper;
-	
-	@Test
-	void UserCanSandMessage() throws Exception {
-		ResponseMessage response = new ResponseMessage("succesfully send message");
-		String responseJson = objectMapper.writeValueAsString(response);
-		api.perform(post("/notification/send?userId=2&message=message+from+test&subject=test+subject"))
-			.andExpect(content().json(responseJson));
-	}
+	/*
+	 * i use manual testing because here nothing to test, i just send messages and test manually
+	 */
 
 }
